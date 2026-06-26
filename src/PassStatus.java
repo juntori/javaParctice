c8dbd0130917b8d1377433c5c5ae3e4988833154
PARKJUNYONG <treeoark1@gmail.com>
2026-06-26 22:17:56 +0900

    enum PassStatus add

--- src/PassStatus.java
+++ src/PassStatus.java
@@ -1,2 +1,14 @@
 public enum PassStatus {
+    PASSED("합격"),
+    FAILED("불합격");
+
+    private final String label;
+
+    PassStatus(String label){
+        this.label = label;
+    }
+
+    String getLabel(){
+        return label;
+    }
 }
