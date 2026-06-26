97b2c496506cb5cfeafed8aa8248e268e8738287
PARKJUNYONG <treeoark1@gmail.com>
2026-06-27 01:26:25 +0900

    Classroom add

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,3 +1,42 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class Classroom {
+    private final List<Student> studentList = new ArrayList<>();
+
+    public Classroom(){
+        this.studentList = new ArrayList<>();
     }
+
+    public void addStudent(Student student){
+        studentList.add(student);
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public Optional<Student> findById(int id){
+        return studentList.stream()
+                .filter(student -> student.getId() == id).findFirst();
+    }
+
+    public List<Student> getPassedStudents(){
+        return studentList.stream()
+                .filter(student -> student.getPsssStatus() == PassStatus.PASSED.getLabel()).toList();
+
+    }
+
+    public Optional<Student> findTopStudent(){
+        return studentList.stream()
+                .max(Comparator.comparingInt(Student::getScore));
+    }
+
+    public List<Student> searchByName(String keyWord){
+        return studentList.stream()
+                .filter(student -> student.getName().equals(keyWorld)).toList();
+    }
+
 }
